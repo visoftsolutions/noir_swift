18e03667413263691bfbddc97dc19343a353ba90
Bartosz Nowak <bartoszn337@gmail.com>
2023-10-24 09:10:45 +0200

    workflow&development&readme

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# noir_swift
+# SWIFT-BRIDGE for `noir_rs`
+
+![GitHub Workflow Status](https://github.com/visoftsolutions/noir_swift/actions/workflows/rust.yml/badge.svg)
+![Version](https://img.shields.io/badge/version-0.1.0-blue)
+
+Bridge the gap between Swift and the `noir_rs` library, offering Swift developers a seamless way to utilize the `noir_rs` library's zkSNARK proving scheme functionalities across iOS, macOS, WatchOS, and other Swift-compatible platforms.
+
+## 🚀 Features
+
+- 📱 Directly use `noir_rs` zkSNARK functionalities in Swift.
+- 🌉 Effortless interfacing between Swift and Rust.
+- 🔒 Secure zkSNARK proof generation and verification.
+
+## 📦 Build
+
+```bash
+cargo build
+```
+
+## 🧪 Testing
+
+Ensure the reliability and robustness of your bridge with the provided tests:
+
+```bash
+cargo test
+```
+
+## 🎯 Quick Usage
+
+We provide two main functions for the bridge:
+
+1. **prove_swift**: Generates a zkSNARK proof using the given circuit bytecode and initial witness.
+2. **verify_swift**: Verifies a given zkSNARK proof against a circuit and its verification key.
+
+```rust
+fn prove_swift(circuit_bytecode: String, initial_witness_vec_raw: Vec<i128>) -> Result<Proof, String>
+fn verify_swift(circuit_bytecode: String, proof: Proof) -> Result<bool, String>
+```
+
+## 🔍 Example
+
+Refer to the `tests` module for a comprehensive usage example:
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use noir_rs::{
+    native_types::{Witness, WitnessMap},
+    prove, verify, FieldElement,
+};
 
 #[swift_bridge::bridge]
 mod ffi {
@@ -8,4 +12,48 @@ mod ffi {
 
 fn hello_rust() -> String {
     String::from("Hello from Rust!")
+}
+
+pub struct Proof {
+    pub proof: Vec<u8>,
+    pub verification_key: Vec<u8>,
+}
+
+pub fn prove_swift(
+    circuit_bytecode: String,
+    initial_witness_vec_raw: Vec<i128>,
+) -> Result<Proof, String> {
+    let initial_witness_vec: Vec<FieldElement> = initial_witness_vec_raw
+        .into_iter()
+        .map(FieldElement::from)
+        .collect();
+    let mut initial_witness = WitnessMap::new();
+    for (i, witness) in initial_witness_vec.into_iter().enumerate() {
+        initial_witness.insert(Witness(i as u32 + 1), witness);
+    }
+
+    let (proof, verification_key) =
+        prove(circuit_bytecode, initial_witness).map_err(|e| e.to_string())?;
+    Ok(Proof {
+        proof,
+        verification_key,
+    })
+}
+
+pub fn verify_swift(circuit_bytecode: String, proof: Proof) -> Result<bool, String> {
+    verify(circuit_bytecode, proof.proof, proof.verification_key).map_err(|e| e.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{prove_swift, verify_swift};
+
+    const BYTECODE: &str = "H4sIAAAAAAAA/7WTMRLEIAhFMYkp9ywgGrHbq6yz5v5H2JkdCyaxC9LgWDw+H9gBwMM91p7fPeOzIKdYjEeMLYdGTB8MpUrCmOohJJQkfYMwN4mSSy0ZC0VudKbCZ4cthqzVrsc/yw28dMZeWmrWerfBexnsxD6hJ7jUufr4GvyZFp8xpG0C14Pd8s/q29vPCBXypvmpDx7sD8opnfqIfsM1RNtxBQAA";
+
+    #[test]
+    fn test_prove_verify() {
+        let proof = prove_swift(String::from(BYTECODE), vec![1_i128, 10_i128]).unwrap();
+        let verdict = verify_swift(String::from(BYTECODE), proof).unwrap();
+        assert!(verdict);
+    }
 }
