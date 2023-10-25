e7d1e269302e6ac8c0982bddfe83e6125de23d5a
Bartosz Nowak <bartoszn337@gmail.com>
2023-10-25 08:48:07 +0200

    proving&verifying in swift works

--- swift/bridging-header.h
+++ swift/bridging-header.h
@@ -1,7 +1,7 @@
 #ifndef BridgingHeader_h
 #define BridgingHeader_h
 
-#include "../generated/noir_swift/noir_swift.h"
 #include "../generated/SwiftBridgeCore.h"
+#include "../generated/noir_swift/noir_swift.h"
 
 #endif /* BridgingHeader_h */
